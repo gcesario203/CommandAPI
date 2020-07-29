c9a4d4c81bd8ecbbb5321f1b75eec6a5d7655cb6
Gabriel Cesario <cesario203@outlook.com>
2020-07-28 21:15:26 -0300

    Pegando as bagulheiras do mock no controller

--- CommandAPI/Controllers/CommandsController.cs
+++ CommandAPI/Controllers/CommandsController.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Models;
+using CommandAPI.Data;
 
 namespace CommandAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/commands")]
     [ApiController]
     public class CommandsController : ControllerBase
     {
+
+        private readonly MockCommanderRepo _mockContext = new MockCommanderRepo();
+
+        //Get api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commands = _mockContext.GetAppCommands();
+
+            return Ok(commands);
+        }
+
+        //GET api/commands/{id} || api/commands/5
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var command = _mockContext.GetCommandById(id);
+
+            return Ok(command);
+        }
     }
 }
